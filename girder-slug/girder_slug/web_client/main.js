9cbd9aa0b843b5b7b4481c89f70e9e6275552f33
Roni Choudhury <roni.choudhury@kitware.com>
2018-11-01 17:01:06 -0400

    Create a "slug" route that looks like it loads a content route

--- girder-slug/girder_slug/web_client/main.js
+++ girder-slug/girder_slug/web_client/main.js
@@ -1,1 +1,10 @@
-console.log('Loaded slug!');
+import router from 'girder/router';
+
+router.route('slug/:id', 'slug', function (slug, params) {
+  const hash = window.location.hash;
+  router.navigate('/collections', {
+    trigger: true,
+    replace: true
+  });
+  router.navigate(hash, { replace: true });
+});
